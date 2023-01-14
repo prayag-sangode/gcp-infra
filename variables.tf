875c0f4d15784cc887cae1a1610035189d3bf233
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2023-01-14 14:10:36 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+
+variable "project_id" {
+  type        = string
+  default = "mypoc-374706"
+}
+
+variable "region" {
+  type        = string
+  default = "asia-south1"
+}
+
+variable "zone" {
+  type        = string
+  default = "asia-south1-b"
+}
