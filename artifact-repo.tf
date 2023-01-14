461abeff4c6fd69ec77abffcd479fd2d071afa27
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2023-01-14 15:24:24 +0530

    Create artifact-repo.tf

---
+++ artifact-repo.tf
@@ -0,0 +1,7 @@
+# Artifact Registry Docker repositories
+resource "google_artifact_registry_repository" "apps-repo" {
+  location      =  var.region
+  repository_id = "apps-repo"
+  description   = "docker repository applications"
+  format        = "DOCKER"
+}
