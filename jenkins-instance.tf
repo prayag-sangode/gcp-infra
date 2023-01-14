3335f7b6d2c2b3d90d7c920f118b887ca9e4e46c
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2023-01-14 18:35:57 +0530

    Update jenkins-instance.tf

--- jenkins-instance.tf
+++ jenkins-instance.tf
@@ -18,7 +18,7 @@ resource "google_compute_instance" "default" {
   }
 
   // metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>My Demo.!</h1></body></html>' | sudo tee /var/www/html/index.html"
-  metadata_startup_script = "sudo apt-get update && sudo apt -y install default-jdk && wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add - && sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list' && sudo apt-get update && sudo apt-get install jenkins -y && sudo systemctl enable --now jenkins && sudo apt -y install xrdp && sudo systemctl enable xrdp"
+  //metadata_startup_script = "sudo apt-get update && sudo apt -y install default-jdk && wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add - && sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list' && sudo apt-get update && sudo apt-get install jenkins -y && sudo systemctl enable --now jenkins && sudo apt -y install xrdp && sudo systemctl enable xrdp"
 
   // Apply the firewall rule to allow external IPs to access this instance
   tags = ["http-server"]
