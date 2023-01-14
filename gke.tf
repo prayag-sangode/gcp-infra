f7bcf128509e21ae8f2553481f63857c2bb3e769
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2023-01-14 19:18:19 +0530

    Update gke.tf

--- gke.tf
+++ gke.tf
@@ -6,5 +6,5 @@ resource "google_container_cluster" "primary" {
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
   #remove_default_node_pool = true
-  initial_node_count       = 1
+  initial_node_count       = 0
 }
