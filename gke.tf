f8535f5694eda25947552eb2e132e1622b3b70ca
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2023-01-17 11:19:54 +0530

    Update gke.tf

--- gke.tf
+++ gke.tf
@@ -1,10 +1,6 @@
 resource "google_container_cluster" "primary" {
   name     = "gke-cluster"
   location = var.zone
-
-  # We can't create a cluster with no node pool defined, but we want to only use
-  # separately managed node pools. So we create the smallest possible default
-  # node pool and immediately delete it.
-  #remove_default_node_pool = true
-  initial_node_count       = 1
+  initial_node_count  = 1
+  machine_type = "e2-micro"
 }
